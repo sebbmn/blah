b91e6049482da2e48aea6a904bc224bb14a0759c
sebbmn <sebbmn@users.noreply.github.com>
2021-10-29 10:35:13 +0200

    feat: setup main layout

--- blah-svelte/src/App.svelte
+++ blah-svelte/src/App.svelte
@@ -1,70 +1,28 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
-  import { WiredButton, WiredInput } from "wired-elements"
 
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-  <wired-input placeholder="Enter name"></wired-input>
-  <wired-button>Click Me</wired-button>
-
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <div id="blah-container">
+    <div class="blah-nav">Navigation</div>
+    <div class="blah-main">Main area</div>
+  </div>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+#blah-container {
+  max-width: 1440px;
+  height: 98vh;
+  margin: 0 auto;
+  display: flex;
+}
+.blah-nav {
+  background-color: lightblue;
+  width: 350px;
+}
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
+.blah-main {
+  background-color: yellow;
+  width: calc(100% - 350px);
+}
 </style>
