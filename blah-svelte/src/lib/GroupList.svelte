058953dea04efbb6a7c652483a914a769fa3d2e2
sebbmn <sebbmn@users.noreply.github.com>
2021-12-07 11:14:30 +0100

    feat: add avatars to group list items

--- blah-svelte/src/lib/GroupList.svelte
+++ blah-svelte/src/lib/GroupList.svelte
@@ -3,6 +3,7 @@ import { WiredCard } from 'wired-elements';
 import { WiredListbox } from 'wired-elements';
 import { WiredIconButton } from 'wired-elements';
 import { navigate } from "svelte-navigator";
+import Avatar from './Avatar.svelte'
 
 export let groupList;
 
@@ -26,7 +27,10 @@ function addGroup(e) {
   <wired-listbox selected="two" class="group-listbox">
     {#each groupList as group}
       <wired-item value={group.id} on:click="{() => navigate(`/group/${group.id}`)}">
-        {group.name}
+        <div class="group-listbox__item">
+          <Avatar image={{src: group.avatar, alt: group.name}} />
+          <div class="group-name">{group.name}</div>
+        </div>
       </wired-item>
 	  {/each}
   </wired-listbox>
@@ -42,8 +46,18 @@ function addGroup(e) {
     overflow-x: hidden;
     margin-top: 20px;
     width: calc(100% - 10px);
-    --wired-item-selected-color: green; 
-    --wired-item-selected-bg: yellowgreen;
+
+
+    &__item {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 5px 0 0 0;
+
+      .group-name {
+        margin-left: 15px;
+      }
+    }
   }
   .add-group {
     position:absolute;
