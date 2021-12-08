c7c69f6a6aeff0044e1c3f5afc2b38ae575dd5a3
sebbmn <sebbmn@users.noreply.github.com>
2021-12-08 10:34:40 +0100

    feat: add message input to the chat

--- blah-svelte/src/lib/Chat.svelte
+++ blah-svelte/src/lib/Chat.svelte
@@ -1,22 +1,44 @@
 <script>
 import { WiredCard } from 'wired-elements';
+import MessageInput from './MessageInput.svelte';
 
 export let messages;
 
 </script>
 
 <wired-card elevation="2" class="chat">
-  <p>chat</p>
-  {#each messages as msg}
-  <p>
-    {msg.message}
-  </p>
-{/each}
+  <div class="chat-top">
+    <p>chat</p>
+    {#each messages as msg}
+      <p>
+        {msg.message}
+      </p>
+    {/each}
+  </div>
+  <div class="chat-bottom">
+    <MessageInput />
+  </div>
 </wired-card>
 
-<style>
+<style lang="scss">
 .chat {
   width: calc(100% - 20px);
   height: calc(100% - 20px);
+  position: relative;
+
+  p {
+    width: 100%;
+  }
+
+  &-top {
+    width: 100%;
+    height: calc(100% - 100px);
+  }
+
+  &-bottom {
+    width: 100%;
+    height: 70px;
+    padding: 0;
+  }
 }
 </style>
---
+++ blah-svelte/src/lib/MessageInput.svelte
@@ -0,0 +1,28 @@
+<script>
+import { WiredCard, WiredInput, WiredIconButton } from 'wired-elements';
+
+</script>
+
+<wired-card elevation="2" class="message-input">
+  <span class="message-input__message">
+    <wired-input class="message-input__message-input">input here...</wired-input>
+  </span>
+  <span class="message-input__send">
+    <wired-icon-button>
+      <mwc-icon>send</mwc-icon>
+    </wired-icon-button>
+  </span>
+</wired-card>
+
+<style lang="scss">
+.message-input {
+  width: calc(100% - 20px);
+  height: 45px;
+
+  &__message {
+    &-input {
+      width: calc(100% - 80px);
+    }
+  }
+}
+</style>
