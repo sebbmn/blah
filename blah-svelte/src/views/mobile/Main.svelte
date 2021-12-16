d1c1b1301dae0056e31060d7d7234c0b5e1616ed
sebbmn <sebbmn@users.noreply.github.com>
2021-12-16 14:24:06 +0100

    refactor: mobile view display

--- blah-svelte/src/views/mobile/Main.svelte
+++ blah-svelte/src/views/mobile/Main.svelte
@@ -25,38 +25,55 @@ function getCurrentChat(id) {
 
 </script>
 
-<Route path="/group/:id" let:params primary={false}>
-  <div class="blah-center-top">
-      <GroupMenubar group={getGroupInfos(params.id)}/>
-  </div>
-  <div class="blah-center-middle">
-      <Chat chat={getCurrentChat(params.id)}/>
-  </div>
-  <div class="blah-center-bottom">
-    <MessageInput />
-  </div>
-</Route>
 <Route path="/" let:params primary={false}>
   <div class="blah-mobile-top">
     <MainMenubar />
   </div>
-  <div class="blah-mobile-middle">
+  <div class="blah-mobile-middle-bottom">
     <GroupList groupList={getGroupList()}/>
   </div>
 </Route>
 
-<style>
-.blah-mobile-top {
-  border: 1px solid white;
-  border-radius: 10px;
-  height: 58px;
-  padding: 5px;
-}
+<Route path="/group/:id" let:params primary={false}>
+  <div class="blah-mobile-top">
+      <GroupMenubar group={getGroupInfos(params.id)}/>
+  </div>
+  <div class="blah-mobile-middle">
+      <Chat chat={getCurrentChat(params.id)}/>
+  </div>
+  <div class="blah-mobile-bottom">
+    <MessageInput />
+  </div>
+</Route>
 
-.blah-mobile-middle {
-  border: 1px solid white;
-  border-radius: 10px;
-  height: calc(100% - 78px);
-  padding: 5px;
+<style lang="scss">
+.blah-mobile {
+  &-top {
+    border: 1px solid white;
+    border-radius: 10px;
+    height: 58px;
+    padding: 5px;
+  }
+
+  &-middle {
+    border: 1px solid white;
+    border-radius: 10px;
+    height: calc(100% - 120px);
+    padding: 5px;
+  }
+
+  &-middle-bottom {
+    border: 1px solid white;
+    border-radius: 10px;
+    height: calc(100% - 43px);
+    padding: 5px;
+  }
+
+  &-bottom {
+    border: 1px solid white;
+    border-radius: 10px;
+    height: 62px;
+    padding: 5px;
+  }
 }
 </style>
