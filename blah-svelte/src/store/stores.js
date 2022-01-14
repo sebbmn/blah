5bef3804f209cf7ee07c5d25280cb21b8c427531
sebbmn <sebbmn@users.noreply.github.com>
2022-01-14 09:30:18 +0100

    refactor: make NewGroup comp a view

--- blah-svelte/src/store/stores.js
+++ blah-svelte/src/store/stores.js
@@ -3,6 +3,7 @@ import { writable } from 'svelte/store';
 export const users = writable([]);
 export const groups = writable([]);
 export const chats = writable([]);
+export const contacts = writable([]);
 export const currentUser = writable({});
 export const currentGroup = writable({});
 export const currentChat = writable({});
--- blah-svelte/src/views/NewGroup.svelte
+++ blah-svelte/src/views/NewGroup.svelte
@@ -1,16 +1,18 @@
 <script>
 import { WiredCard, WiredIconButton } from 'wired-elements';
 import { createEventDispatcher } from 'svelte';
+import { contacts } from '../store/stores.js';
 
 const dispatch = createEventDispatcher();
 
 function navigateBack() {
+  console.log($contacts)
   dispatch('navigateBack', {});
 }
 </script>
 
 <wired-card elevation="2" class="blah-new-group">
-  new group form
+  new chat
   <wired-icon-button class="blah-new-group__back-button" on:click={navigateBack}>
     <mwc-icon>arrow_back</mwc-icon>
   </wired-icon-button>
