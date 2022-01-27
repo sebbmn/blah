02896278d225b4b312ff348a1270930eaa494b6b
sebbmn <sebbmn@users.noreply.github.com>
2022-01-27 10:14:38 +0100

    feat: impl. add contact to a groups

--- blah-svelte/src/lib/NewGroup.svelte
+++ blah-svelte/src/lib/NewGroup.svelte
@@ -1,15 +1,30 @@
 <script>
-export let contacts = [];
-</script>
-<div class="blah-new-group">New Group...</div>
-{#each contacts as contact }
-  <div>{contact.name}</div>
-{/each}
+import ContactList from '../lib/ContactList.svelte';
 
+export let contacts = [];
+
+const group = {
+  name: '',
+  description: '',
+  avatar: '',
+  groupType: 'group',
+  lastMessage: '',
+  members: []
+};
+
+function addMember(member) {
+  const isNewMember = !group.members.some(mbr => mbr.id === member.id);
+
+  group.members = isNewMember ? [...group.members, member] : group.members;
+}
+</script>
+
+<div class="blah-new-group">New Group...
+  <ContactList contactList={contacts} on:selectContact={(e) => addMember(e.detail.contact)}/>
+</div>
 
 <style style lang="scss">
 .blah-new-group {
-  background-color: cornflowerblue;
   margin-top: 20px;
 }
 </style>
