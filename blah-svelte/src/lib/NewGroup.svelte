38df49e110745112f3414586667e95b8e2a409f3
sebbmn <sebbmn@users.noreply.github.com>
2022-03-04 09:38:41 +0100

    feat: disp. members added to new group

--- blah-svelte/src/lib/NewGroup.svelte
+++ blah-svelte/src/lib/NewGroup.svelte
@@ -16,10 +16,22 @@ function addMember(member) {
   const isNewMember = !group.members.some(mbr => mbr.id === member.id);
 
   group.members = isNewMember ? [...group.members, member] : group.members;
+  group.members = group.members;
+
 }
 </script>
 
 <div class="blah-new-group">New Group...
+  <p>
+    {#each group.members as member, i}
+      {member.name}
+      {#if i < group.members.length - 1}
+        <span>,&nbsp;</span>
+      {/if}
+    {:else}
+      Add members to the list...
+    {/each}
+  </p>
   <ContactList contactList={contacts} on:selectContact={(e) => addMember(e.detail.contact)}/>
 </div>
 
