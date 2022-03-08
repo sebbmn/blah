80659b8ec743bb61de09f9fbcdfae06b8c1a0bef
sebbmn <sebbmn@users.noreply.github.com>
2022-03-08 15:54:43 +0100

    feat: impl. new contact comp.

--- blah-svelte/src/lib/NewContact.svelte
+++ blah-svelte/src/lib/NewContact.svelte
@@ -1,12 +1,19 @@
 <script>
-export let users = [];
+import { WiredInput } from 'wired-elements';
+
+let id;
 </script>
 
-<div class="blah-new-contact">New contact...</div>
+<div class="blah-new-contact">
+  <wired-input class="blah-new-contact__name" bind:this="{id}" placeholder="Enter contact id"></wired-input>
+</div>
 
 <style style lang="scss">
 .blah-new-contact {
-  background-color: cornflowerblue;
   margin-top: 20px;
+
+  &__name {
+    width: calc(100% - 20px);
+  }
 }
 </style>
