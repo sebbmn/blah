b781f4c49d009e51aea65f62cc817017da8ccd50
sebbmn <sebbmn@users.noreply.github.com>
2022-04-22 12:27:23 +0200

    feat: publish gh-pages branch

--- src/store/stores.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const users = writable([]);
-export const groups = writable([]);
-export const chats = writable([]);
-export const contacts = writable([]);
-export const currentUser = writable({});
-export const currentGroup = writable({});
-export const currentChat = writable({});
