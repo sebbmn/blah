2dafba0270c355979d6809a0c76e8a1a41bb0211
sebbmn <sebbmn@users.noreply.github.com>
2022-04-28 12:11:36 +0200

    feat: add group info dialog

--- src/lib/GroupMenubar.svelte
+++ src/lib/GroupMenubar.svelte
@@ -1,10 +1,12 @@
 <script>
-import { WiredCard, WiredIconButton } from 'wired-elements';
+import { WiredCard, WiredIconButton, WiredDialog } from 'wired-elements';
 import { createEventDispatcher } from 'svelte';
 import Avatar from './Avatar.svelte';
 
 export let group;
 export let isMobile = false;
+
+let showGroupInfo = false;
 
 const dispatch = createEventDispatcher();
 
@@ -12,23 +14,36 @@ function navigateBack() {
   dispatch('navigateBack', {});
 }
 
-function openGroupInfos() {
-  dispatch('openGroupInfos', {});
+function toggleGroupInfos() {
+  showGroupInfo = !showGroupInfo;
 }
 </script>
-  
+
+<wired-dialog open={showGroupInfo}>
+  <div class="blah-group-menubar__group-infos">
+    <div>
+      <h3>{group.name}</h3>
+    </div>
+    <div>
+      <img src={group.avatar} alt={group.name} style="max-height: 30vh;" />
+    </div>
+    <div style="width:100%; text-align:center;">
+      <wired-button id="closeDialog" on:click={toggleGroupInfos}>Close</wired-button>
+    </div>
+  </div>
+</wired-dialog>
+
 <wired-card elevation="2" class="blah-group-menubar">
   <div class="blah-group-menubar__container">
     {#if isMobile}
       <mwc-icon class="blah-group-menubar__icon" on:click={navigateBack}>arrow_back</mwc-icon>
     {/if}
-    <div class="blah-group-menubar__infos" on:click={openGroupInfos}>
+    <div class="blah-group-menubar__infos" on:click={toggleGroupInfos} title="Group infos">
       <div class="blah-group-menubar__avatar">
         <Avatar image={{src: group.avatar, alt: group.name}} />
       </div>
       <p>{group.name}</p>
     </div>
-    <!--<mwc-icon class="blah-group-menubar__icon">more_vert</mwc-icon>-->
   </div>
 </wired-card>
   
@@ -37,6 +52,17 @@ function openGroupInfos() {
   width: 100%;
   height: 100%;
   padding: 0;
+
+  &__group-infos {
+    width: 300px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    span {
+      margin: auto;
+    }
+  }
   &__container {
     width: calc(100% - 25px);
     height: 58px;
